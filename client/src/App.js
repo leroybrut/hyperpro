4d368425fe172f90d5eb7ff4def89dce9324a734
Leroy Davidson Descelles <leroy.davidsondescelles@zoolt.com>
2021-03-03 10:37:46 +0100

    fix for node modules

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import Login from './Components/Login/Login';
 import UserContext from './Context/UserContext';
+import { Helmet } from 'react-helmet';
 
 function App() {
   const test = null;
   return (
     <>
+    <Helmet>
+      <title>Hyperpro - Login</title>
+    </Helmet>
     <UserContext.Provider value={test}>
     <div className="App">
       <Login />
